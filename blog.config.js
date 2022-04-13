0ae6b19a6fb0b4a1eb1107979c342a55400895d0
Lemonawa <lemonawa1209@gmail.com>
2022-04-13 13:50:57 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -25,25 +25,25 @@ const BLOG = {
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
-    provider: '', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
+    provider: 'ga', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
     ackeeConfig: {
       tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
       dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
       domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: '' // e.g: G-XXXXXXXXXX
+      measurementId: 'G-W9D7JE40V0' // e.g: G-XXXXXXXXXX
     }
   },
   comment: {
     // support provider: gitalk, utterances, cusdis
-    provider: '', // leave it empty if you don't need any comment plugin
+    provider: 'gitalk', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
-      repo: '', // The repository of store comments
-      owner: '',
-      admin: [],
-      clientID: '',
-      clientSecret: '',
+      repo: 'nobelium', // The repository of store comments
+      owner: 'Lemonawa',
+      admin: [Lemonawa],
+      clientID: 'd428c6e4b1c19d35a4e9',
+      clientSecret: 'e840a379d4a3eb51688ff436f19cd93ea3964b9f',
       distractionFreeMode: false
     },
     utterancesConfig: {
